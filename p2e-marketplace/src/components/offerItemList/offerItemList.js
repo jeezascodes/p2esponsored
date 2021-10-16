a07ff5f2c30f241fb6aaa133a4046e3735e461ac
jesus <macbookpro@MacBook-Pro.local>
2021-10-16 02:11:42 -0500

    Offer list

--- p2e-marketplace/src/components/offerItemList/offerItemList.js
+++ p2e-marketplace/src/components/offerItemList/offerItemList.js
@@ -10,8 +10,28 @@ export default function OfferItemList({ item = { image: '' } }) {
             <img src={item.thumbnail} className={styles.thumbnail} />
             <img src={item.thumbnail} className={styles.thumbnailBg} />
           </div>
-          <div className={styles.middle}>middle</div>
-          <div className={styles.right}>right</div>
+          <div className={styles.middle}>
+            <div className={styles.infoContainer}>
+              <p className={styles.username}>{item.username}</p>
+              <p className={styles.gamename}>{item.game}</p>
+              <p className={styles.description}>{item.description}</p>
+              <p className={styles.date}>updated at: {item.updated_at}</p>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.infoDevider}>
+              <p className={styles.infoTitle}>Potential monthly earnings</p>
+              <p className={styles.infoText}>300 USD</p>
+            </div>
+            <div>
+              <p className={styles.infoTitle}>Percentage for scholar</p>
+              <p className={styles.infoText}>50%</p>
+            </div>
+            <div>
+              <p className={styles.infoTitle}>Places left</p>
+              <p className={styles.infoText}>5</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- p2e-marketplace/src/pages/offers/offers.js
+++ p2e-marketplace/src/pages/offers/offers.js
@@ -7,10 +7,18 @@ import Heading from '../../components/heading/heading';
 export default function Offers() {
   const offers = [
     {
+      username: 'jeezascodes',
+      game: 'Axie infinity',
+      description: 'Scholarship duration: 3 months',
+      updated_at: '2021-7-21',
       thumbnail:
         'https://media.ambito.com/p/db25c95083c5e472869cfeb92fea1ffd/adjuntos/239/imagenes/039/293/0039293731/1200x1200/smart/axie-infinityjpg.jpg',
     },
     {
+      username: 'jeezascodes',
+      game: 'Zed run',
+      description: 'Scholarship duration: undefined',
+      updated_at: '2021-7-21',
       thumbnail:
         'https://near.org/wp-content/uploads/2020/09/cs_zed_header.png',
     },

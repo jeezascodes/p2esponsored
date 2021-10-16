9f1f5e94d067a7879533934ea24b6d44c0f445b7
jesus <macbookpro@MacBook-Pro.local>
2021-10-16 12:58:40 -0500

    Select component and create offer

--- p2e-marketplace/src/components/createOfferForm/createOfferForm.js
+++ p2e-marketplace/src/components/createOfferForm/createOfferForm.js
@@ -1,63 +1,85 @@
 import React, { useState } from 'react';
 import styles from './createOfferForm.module.css';
 import TextInput from '../../components/textInput/textInput';
+import SelectInput from '../../components/selectInput/selectInput';
 import PrimaryButton from '../../components/button/button';
 import { loginUser } from '../../data/apiCalls';
 import { useHistory } from 'react-router-dom';
 import Loader from 'react-loader-spinner';
 
 export default function CreateOfferForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [game, setGame] = useState('');
+  const [otherGame, setOtherGame] = useState('');
+  const [percentage, setPercentage] = useState(10);
+  const [duration, setDuration] = useState(1);
+  const [expectations, setExpectations] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
   const onValueChange = (event, id) => {
-    if (id === 'name') setName(event.target.value);
-    if (id === 'email') setEmail(event.target.value);
-    if (id === 'password') setPassword(event.target.value);
+    if (id === 'other_game') setOtherGame(event.target.value);
+    if (id === 'percentage') setPercentage(event.target.value);
+    if (id === 'expectations') setExpectations(event.target.value);
   };
+
+  const gemeOptions = [
+    { value: 'Axie infinity', label: 'Axie infinity' },
+    { value: 'Zed run', label: 'Zed run' },
+    { value: 'Binamon', label: 'Binamon' },
+    { value: 'Plant vs Undead', label: 'Plant vs Undead' },
+    { value: 'Other', label: 'Other' },
+  ];
+
+  const options = [
+    { value: 'undefined', label: 'undefined duration' },
+    { value: 1, label: '1 month' },
+    { value: 2, label: '2 months' },
+    { value: 3, label: '3 months' },
+  ];
 
   const onSubmit = async () => {
     setIsLoading(true);
-    const data = {
-      username: name,
-      email,
-      password,
-    };
-    try {
-      const response = await loginUser(data);
-      setIsLoading(false);
-      history.push('/offers');
-    } catch (err) {
-      setIsLoading(false);
-      console.log(`err`, err);
-    }
   };
 
   return (
     <div className={styles.form}>
-      <TextInput
+      <SelectInput
+        name='Game'
+        value={game}
+        options={gemeOptions}
+        onChange={setGame}
+      />
+      {game.label === 'Other' && (
+        <TextInput
+          name='Which game?'
+          placeholder='game name'
+          value={otherGame}
+          id='other_game'
+          onChange={onValueChange}
+          type='text'
+        />
+      )}
+      <SelectInput
         name='Scholarship duration (in months)'
-        placeholder='username'
-        value={name}
-        id='name'
-        onChange={onValueChange}
+        value={duration}
+        options={options}
+        onChange={setDuration}
       />
       <TextInput
-        placeholder='email'
-        id='email'
-        value={email}
+        name='Earnings percentage for the scholar'
+        placeholder='50'
+        value={percentage}
+        id='percentage'
         onChange={onValueChange}
-        type='email'
+        type='text'
       />
       <TextInput
-        placeholder='password'
-        value={password}
-        id='password'
+        name='Monthly Earnings expections for the scholar (in USD)'
+        placeholder='200'
+        value={expectations}
+        id='expectations'
         onChange={onValueChange}
-        type='password'
+        type='text'
       />
       {isLoading ? (
         <Loader type='Puff' color='#00BFFF' height={50} width={50} />

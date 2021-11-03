503f1e4b6c6af5a9b215672cab204d48d6517d04
jesus <macbookpro@MacBook-Pro.local>
2021-11-02 23:20:05 -0600

    add props to cards

--- p2e-marketplace/src/components/chooseCard/chooseCard.js
+++ p2e-marketplace/src/components/chooseCard/chooseCard.js
@@ -3,19 +3,14 @@ import styles from './chooseCard.module.css';
 import PrimaryButton from '../../components/button/button';
 import Heading from '../../components/heading/heading';
 import { Link } from 'react-router-dom';
-import leftVideo from '../../assets/astronaut_user.mp4';
 
-export default function ChooseCard() {
+export default function ChooseCard({ data }) {
   return (
     <div>
       <div className={styles.cardWrapper}>
         <div className={styles.thumbnailContainer}>
-          {/* <img
-            src='https://api.thegraph.com/ipfs/api/v0/cat?arg=QmP4AbpQXQH2gJeEY1KCtjUBnroiEksWBHH9A9zMBgrdZ8'
-            className={styles.thumbnail}
-          /> */}
           <video className={styles.thumbnail} autoPlay loop muted>
-            <source src={leftVideo} type='video/mp4' />
+            <source src={data?.videoSource} type='video/mp4' />
           </video>
           <img
             src='https://api.thegraph.com/ipfs/api/v0/cat?arg=QmP4AbpQXQH2gJeEY1KCtjUBnroiEksWBHH9A9zMBgrdZ8'
@@ -23,16 +18,8 @@ export default function ChooseCard() {
           />
         </div>
         <div className={styles.textContainer}>
-          <Heading
-            text='Scholarship sponsor'
-            herarchy='h3'
-            marginBottom={false}
-          />
-          <Heading
-            text='I will play with someone elses account and will split the profits'
-            herarchy='h4'
-            bold={false}
-          />
+          <Heading text={data?.title} herarchy='h3' marginBottom={false} />
+          <Heading text={data?.subtitle} herarchy='h4' bold={false} />
           <p></p>
         </div>
       </div>
--- p2e-marketplace/src/pages/createProfile/createProfile.js
+++ p2e-marketplace/src/pages/createProfile/createProfile.js
@@ -5,14 +5,27 @@ import MainView from '../../components/mainView/mainView';
 import Heading from '../../components/heading/heading';
 import { useHistory } from 'react-router-dom';
 import { getOffers, getAllGames } from '../../data/apiCalls';
+import scholarVideo from '../../assets/astronaut_user.mp4';
+import investorVideo from '../../assets/astronaut_investor.mp4';
 
 export default function CreateProfile() {
+  const scholar = {
+    videoSource: scholarVideo,
+    title: 'Scholar',
+    subtitle:
+      "I want to play with somebody else's account and split the profits",
+  };
+  const investor = {
+    videoSource: investorVideo,
+    title: 'Investor',
+    subtitle: 'I want pay the entrance cost and sit down to collect profits',
+  };
   return (
     <MainView>
       <Heading text='Which one are you?' herarchy='h3' />
       <section className={styles.cardsContainer}>
-        <ChooseCard />
-        <ChooseCard />
+        <ChooseCard data={scholar} />
+        <ChooseCard data={investor} />
       </section>
     </MainView>
   );
